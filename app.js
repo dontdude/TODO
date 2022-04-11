4d573afa5d9cfd230cb43232f0647fd8bef70bda
Chandan Mishra <imchandan1947@gmail.com>
2022-04-11 19:50:53 +0530

    Changing styles and deploying

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post("/reset", function(req, res){
     res.redirect("/");
 })
 
-app.listen(3000, function(){
+app.listen(process.env.port || 3000, function(){
    console.log("Server is running on port 3000");
 });
 
